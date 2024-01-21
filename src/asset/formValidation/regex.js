dfc7b1e3d5b5f2bf250b5fe878a04d9ef07cc9a4
Jason Selau Kumar <148664294+Mecha-Coder@users.noreply.github.com>
2024-01-21 08:18:34 +0800

    setup backup

--- src/asset/formValidation/regex.js
+++
@@ -1,6 +0,0 @@
-
-export const USER_REGEX  = /^[A-Z][A-Za-z0-9_-]{4,}$/
-export const EMAIL_REGEX = /^[^\s@]{3,}@(gmail|yahoo|outlook|hotmail|aol|protonmail|zoho|icloud|mail|yandex)\.(com|net|org|edu|gov|mil|int|io|info)$/
-export const PSW_REGEX   = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
-
-
--- src/components/Buttons.jsx
+++
@@ -1,6 +0,0 @@
-export function FormButton({onClick, disable}){
-  return (
-    <button disabled={disable} onClick={()=>{onClick()}}>Sign Up</button>
-  );
-}
-
